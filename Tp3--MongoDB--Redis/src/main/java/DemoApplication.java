6f98c114db630fe36f8e6afcea5b571074abe3ef
yannAlexCloud <yannattoube@gmail.com>
2019-03-08 20:00:32 +0100

    Implémentation de la partie Redis

--- Tp3--MongoDB--Redis/src/main/java/DemoApplication.java
+++ Tp3--MongoDB--Redis/src/main/java/DemoApplication.java
@@ -1,6 +1,7 @@
 import com.mongodb.MongoClient;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
+import redis.clients.jedis.Jedis;
 
 
 public class DemoApplication {
@@ -55,6 +56,67 @@ public class DemoApplication {
             }
 
         }
+
+
+
+        //Implémentation Redis
+
+
+        //Exemple1
+        Jedis jedis = new Jedis("localhost");
+        jedis.set("foo", "bar");
+        String value = jedis.get("foo");
+
+        System.err.println(value);
+
+        //EXemple 2
+        /*
+        Jedis jedis = new Jedis("localhost");
+        System.out.println(jedis.get("counter"));
+        jedis.incr("counter");
+        System.out.println(jedis.get("counter"));
+        */
+
+        //Exemple 3
+        /*
+        String cacheKey = "cachekey";
+        Jedis jedis = new Jedis("localhost");
+        // adding a new key
+        jedis.set(cacheKey, "cached value");
+        // setting the TTL in seconds
+        jedis.expire(cacheKey, 15);
+        // Getting the remaining ttl
+        System.out.println("TTL:" + jedis.ttl(cacheKey));
+        Thread.sleep(1000);
+        System.out.println("TTL:" + jedis.ttl(cacheKey));
+        // Getting the cache value
+        System.out.println("Cached Value:" + jedis.get(cacheKey));
+
+        // Wait for the TTL finishs
+        Thread.sleep(15000);
+
+        // trying to get the expired key
+        System.out.println("Expired Key:" + jedis.get(cacheKey));
+        */
+
+        //Exemple4
+        /*
+        String cacheKey = "languages";
+        Jedis jedis = new Jedis("localhost");
+        // Adding a set as value
+
+        jedis.sadd(cacheKey, "Java");
+        jedis.sadd(cacheKey, "C#");
+        jedis.sadd(cacheKey, "Python");// SADD
+
+        // Getting all values in the set: SMEMBERS
+        System.out.println("Languages: " + jedis.smembers(cacheKey));
+        // Adding new values
+        jedis.sadd(cacheKey, "Java");
+        jedis.sadd(cacheKey, "Ruby");
+        // Getting the values... it doesn't allow duplicates
+        System.out.println("Languages: " + jedis.smembers(cacheKey));
+        */
 
     }
 
